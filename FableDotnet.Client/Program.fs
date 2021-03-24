9ab20274fa2a78044b1e27c8732ead6bcba5643c
Ito, Kai <kai.ito@zuehlke.com>
2021-03-24 15:15:28 +0100

    Doesn't work with messagepack

--- FableDotnet.Client/Program.fs
+++ FableDotnet.Client/Program.fs
@@ -1,35 +1,38 @@
 ﻿open Fable.SignalR
 open FableShared
 open Microsoft.Extensions.Logging
+open Microsoft.AspNetCore.TestHost
+open Microsoft.Extensions.Hosting
+open Program
 
 [<EntryPoint>]
 let main argv =
-    let hub =
-        SignalR.Connect<SignalRHub.Action, _, _, SignalRHub.Response, _>(fun hub ->
-            hub
-                .WithUrl(sprintf "http://127.0.0.1:5000%s" Endpoints.Root)
-                //.UseMessagePack()
-                .ConfigureLogging(fun l -> l.SetMinimumLevel(LogLevel.Debug)))
-
-    // This works
     async {
+        let! host =
+            App.app
+                .ConfigureWebHost(fun webBuilder -> webBuilder.UseTestServer() |> ignore)
+                .StartAsync()
+            |> Async.AwaitTask
+
+        let testServer = host.GetTestServer()
+
+        let hub: HubConnection<SignalRHub.Action, _, _, SignalRHub.Response, _> =
+            SignalR.Connect<SignalRHub.Action, _, _, SignalRHub.Response, _>(fun hub ->
+                hub
+                    .WithUrl(sprintf "http://127.0.0.1:5000%s" Endpoints.Root,
+                             (fun o -> o.HttpMessageHandlerFactory <- (fun _ -> testServer.CreateHandler())))
+                    .ConfigureLogging(fun l -> l.SetMinimumLevel(LogLevel.Debug))
+                    .UseMessagePack())
+            
         do! hub.Start()
         printfn "ConnectionID: {%O}, State: {%O}" hub.ConnectionId hub.State
 
         let! res = hub.Invoke <| SignalRHub.Action.OnConnect 1
         printfn "responsed: {%O}" res
+        do! Async.Sleep(1000)
+        
+        printfn "-------"
 
-        do! hub.Stop()
-        printfn "ConnectionID: {%O}, State: {%O}" hub.ConnectionId hub.State
-    }
-    |> Async.RunSynchronously
-
-    printfn "-------"
-
-    // This works now
-    async {
-        do! hub.Start()
-        printfn "ConnectionID: {%O}, State: {%O}" hub.ConnectionId hub.State
 
         do! hub.Send <| SignalRHub.Action.OnConnect 1
 
@@ -38,8 +41,7 @@ let main argv =
 
         printfn "%O" a
 
-        // This was the key missing piece :(
-        do! Async.Sleep(2000)
+        do! Async.Sleep(1000)
 
         do! hub.Stop()
         printfn "ConnectionID: {%O}, State: {%O}" hub.ConnectionId hub.State
--- FableDotnet.Server/Program.fs
+++ FableDotnet.Server/Program.fs
@@ -1,57 +1,40 @@
 ﻿open System
 open Fable.SignalR
-open Microsoft.AspNetCore.Builder
-open Microsoft.AspNetCore.Hosting
-open Microsoft.Extensions.Hosting
-open Microsoft.Extensions.DependencyInjection
-open Giraffe
-open Microsoft.Extensions.Logging
+open Saturn
+open FSharp.Control.Tasks.V2
+open FableShared.SignalRHub
 
 module SignalRHub =
-    open Fable.SignalR
-    open FSharp.Control.Tasks.V2
-    open FableShared.SignalRHub
-
     let update (msg: Action) =
         match msg with
         | Action.OnConnect i -> Response.Connected(sprintf "{%i} connected" i)
 
     let invoke (msg: Action) (hubContext: FableHub) =
-        // This works
         printfn "invoked msg: {%O}" msg
         task { return update msg }
 
     let send (msg: Action) (hubContext: FableHub<Action, Response>) =
-        // This never gets called
         printfn "Recevied msg: {%O}" msg
         update msg |> hubContext.Clients.Caller.Send
 
-let webApp = choose [ route "/ping" >=> text "pong" ]
-
-let settings =
-    { SignalR.Settings.EndpointPattern = FableShared.Endpoints.Root
-      SignalR.Settings.Send = SignalRHub.send
-      SignalR.Settings.Invoke = SignalRHub.invoke
-      SignalR.Settings.Config =
-          Some
-              { SignalR.Config.Default<FableShared.SignalRHub.Action, FableShared.SignalRHub.Response>() with
-                    UseMessagePack = false } }
-
-type Startup() =
-    member __.ConfigureServices(services: IServiceCollection) =
-        services.AddGiraffe() |> ignore
-        services.AddSignalR(settings) |> ignore
-
-    member __.Configure (app: IApplicationBuilder) (env: IHostEnvironment) (loggerFactory: ILoggerFactory) =
-        app.UseSignalR(settings) |> ignore
-        app.UseGiraffe webApp
-
-[<EntryPoint>]
-let main _ =
-    Host
-        .CreateDefaultBuilder()
-        .ConfigureWebHostDefaults(fun webHostBuilder -> webHostBuilder.UseStartup<Startup>() |> ignore)
-        .Build()
-        .Run()
-
-    0
+module App =
+    let app =
+        application {
+            url "http://0.0.0.0:5000"
+            no_router
+            use_developer_exceptions
+            use_signalr (
+                configure_signalr{
+                    endpoint FableShared.Endpoints.Root
+                    send SignalRHub.send
+                    invoke SignalRHub.invoke
+                    use_messagepack
+                    with_hub_options (fun ho -> ho.EnableDetailedErrors <- Nullable<bool>(true))
+                }
+            )
+        }
+    [<EntryPoint>]
+    let main _ =
+        run app
+    
+        0
